292c74ff59ebc0916fde799b6917c5526b983c3e
Gerrit Weiermann <gerrit.weiermann@gmx.de>
2025-04-24 14:46:44 +0200

    add loader function for connecting labels with images

---
+++ load_dataset.py
@@ -0,0 +1,29 @@
+import os
+import json
+
+def load_dataset(path):
+    partial_message = " (roof only partially visible)"
+    result = []
+    for root, dirs, files in os.walk(os.path.join(os.getcwd(), path, '2_amount_of_panels')):
+        for file in files:
+            if file.endswith('.json'):
+                # print("[INFO] Loading file:", file)
+                with open(os.path.join(root, file), 'r') as f:
+                    data = json.loads(f.read())
+                    for response in data['data']['responses']:
+                        for index, attachment in enumerate(response['attachments']):
+                            raw_number = attachment['number_of_modules']
+                            if raw_number == 'not applicable':
+                                print(f"[WARN] Skipping attachment {index} of file {file} because number_of_modules is 'not applicable'")
+                                continue
+                            is_partial = raw_number.endswith(partial_message)
+                            number = int(raw_number, 10) if not is_partial else int(raw_number[:-len(partial_message)], 10)
+                            result.append({
+                                'projectId': data['projectId'],
+                                'imageIndex': index,
+                                'numberOfModules': number,
+                                'isPartial': is_partial,
+                                'imagePath': os.path.join(os.getcwd(), path, 'dataset', file),
+                            })
+    return result
+    
---
+++ main.py
@@ -0,0 +1,12 @@
+import pandas as pd
+from load_dataset import load_dataset
+
+if __name__ == "__main__":
+    # Load the dataset using the function from load_dataset.py
+    dataset = load_dataset('dataset')
+    
+    # Convert the dataset to a DataFrame for better visualization
+    df = pd.DataFrame.from_dict(dataset)
+    
+    # Display the first few rows of the DataFrame
+    print(df.head())
